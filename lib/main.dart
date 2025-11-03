c189f608238cd56bcbf54b0ffd52a09a38aeccae
Steve Beville <stevebeville@proton.me>
2025-11-03 23:51:44 +0000

    begin adding Supabase for cloud backup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:sr_language_tool/locator.dart';
 import 'package:sr_language_tool/pages/home_view.dart';
 import 'package:sr_language_tool/services/card_cubit.dart';
@@ -11,6 +12,7 @@ final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   setupLocator();
   DatabaseService().initialiseDB();
   runApp(const MyApp());
